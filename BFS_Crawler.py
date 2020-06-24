cb7bf096818f542457f92f069c5442084c1263bb
dark-knight1729 <giftykhurana0@gmail.com>
2020-06-24 15:32:15 +0530

    Output Written to log

--- BFS_Crawler.py
+++ BFS_Crawler.py
@@ -13,49 +13,49 @@ class link:
     soup=BeautifulSoup(htmlCode,'html.parser')
     for url in soup.findAll('a'):
       href=url.get('href')
-      if(href and href[0]!='#' and href[0]!='/'):
+      if(href and href[:4]=="http"):
         self.linksOnPage.append(href)
 
     text=" ".join(soup.strings)
-    text=text.strip()
+    text=text.split()
     wordFrequency=Counter(text)
     for word in wordFrequency:
-      if(word=='' or word==' ' or word=='\n'):
+      if word=='' or word==' ' or word=='\n':
         continue
       if self.mostFrequentWord is None:
         self.mostFrequentWord=(word,wordFrequency[word])
       elif wordFrequency[word]>self.mostFrequentWord[1]:
         self.mostFrequentWord=(word,wordFrequency[word])
     if self.mostFrequentWord is None:
-      self.mostFrequentWord('NA',-1)
+      self.mostFrequentWord=('NA',-1)
 
-
-def crawler(startURL,limit):
+def outputToFileScreen(message):
+  print(message)
+  print(message,file=file)
+def crawler(startURL,limit,timeout=10):
   assert isinstance(startURL,str),'URL Not in string format'
   visited=set()
   listOfLinks=[]
-  print("Press Ctrl-C to Stop Crawling before limit\n")
-  print("Crawling Started\n")
+  outputToFileScreen("Crawling Started\n")
   queue=deque()
   queue.append(startURL)
   queue.append('-1')
   visited.add(startURL)
   depth=0
   while len(queue)>0 and (len(listOfLinks)<limit or limit==-1):
-    try:
-      url=queue[0]
-      queue.popleft()
-      if url=='-1':
-        depth+=1
-        if(len(queue)!=0):
-          queue.append('\n')
-        continue
+    url=queue[0]
+    queue.popleft()
+    if url=='-1':
+      depth+=1
+      if(len(queue)!=0):
+        queue.append('-1')
+    else:
       try:
-        print("Trying To parse {}".format(url))
-        htmlCode=requests.get(url).text
-        print("Completed Parsing {}\n".format(url))
+        outputToFileScreen("Trying To parse {}".format(url))
+        htmlCode=requests.get(url,timeout=timeout).text
+        outputToFileScreen("Completed Parsing {}\n".format(url))
       except:
-        print("Unable To parse {}\n".format(url))
+        outputToFileScreen("Unable To parse {}\n".format(url))
         continue
       Link=link(url,depth)
       Link.extractDetails(htmlCode)
@@ -64,18 +64,25 @@ def crawler(startURL,limit):
         if eachLink not in visited:
           queue.append(eachLink)
           visited.add(eachLink)
-    except KeyboardInterrupt:
-      print("Crawling Stopped\n")
-      return listOfLinks
-      
-  print("Finished Crawling\n")
+
+  outputToFileScreen("Finished Crawling\n")
   return listOfLinks
 
 if __name__=='__main__':
-  startURL=input("Enter Starting Url(Start with http:// or https://): ")
-  maxNumberOfLinksToParse=int(input("Enter Max number of Links to parse(-1 for No Limit): "))
+  file=open("log.txt",'w')
+
+  try:
+    startURL=input("Enter Starting Url(Start with http:// or https://): ")
+    maxNumberOfLinksToParse=int(input("Enter Max number of Links to parse(-1 for No Limit): "))
+    timeout=int(input("Enter Maximum time for a page(in seconds):"))
+  except:
+    print("Invalid Input")
+    exit()
+
   listOfLinks=crawler(startURL,maxNumberOfLinksToParse)
-  print("Result")
-  print("Total Pages Parsed: {}\n".format(len(listOfLinks)))
+
+  outputToFileScreen("Result")
+  outputToFileScreen("Total Pages Parsed: {}\n".format(len(listOfLinks)))
+
   for link in listOfLinks:
-    print("URL: {}\nDepth: {}\nMost Frequent Word: {}\nNumber of Links on Page: {}\n".format(link.url,link.depth,link.mostFrequentWord,len(link.linksOnPage)))
+    outputToFileScreen("URL: {}\nDepth: {}\nMost Frequent Word: {}\nNumber of Links on Page: {}\n".format(link.url,link.depth,link.mostFrequentWord,len(link.linksOnPage)))
